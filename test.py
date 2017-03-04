3dc4da3a9f1b0fee2e4e56529680c769021bf497
Clive Chan <cc@clive.io>
2017-03-03 22:16:47 -0500

    working on waypointing

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import socket
 import sys
 import time
 
+
 HOST, PORT = "codebb.cloudapp.net", 17429
 LOGIN = "duckduckgoose goosegooseduck"
 #HOST, PORT = "10.20.42.146", 17429
@@ -17,20 +18,42 @@ SCOREBOARD
 CONFIGURATIONS
 """
 
-#def waypoint():
-  
+{
+  "player": (x,y),
+  "mines": []
+}
+class Player:
+  def __init__(self, HOST, PORT, LOGIN):
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.sock.connect((HOST, PORT))
+    print('LOGIN')
+    self.sock.send((LOGIN+'\n').encode())
+    print('success');
+  def getRawArr(self):
+    self.sock.send('STATUS\n'.encode())
+    data = self.sock.recv(4096)
+    arr = repr(data).split(' ')
+    retval = dict()
+    retval["playerpos"] = (float(arr[1]), float(arr[2]))
+    retval["playervel"] = (float(arr[3]), float(arr[4]))
+    
+    int(arr[6]) #number of mines
+    (float(arr[8+3*i]), float(arr[9+3*i]))
+    retval["mines"] = minearray
+  def getPositionVelocity(self):
+    arr = self.getRawArr()
+    return (float(arr[1]), float(arr[2]), float(arr[3]), float(arr[4]))
+  def getNearestMine(self):
+    arr = self.getRawArr()
+    return (float(
+  def waypoint(self, x, y): #fly through this point exactly. blocks until done.
+    ##while not passed through:
+    pass
 
 try:
-  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  sock.connect((HOST, PORT))
-  
-  print('> [LOGIN]')
-  sock.send((LOGIN+'\n').encode())
+  p = Player(HOST, PORT, LOGIN)
   while True:
-    print('> STATUS');
-    sock.send('STATUS\n'.encode())
-    data = sock.recv(4096)
-    print(repr(data))
-except:
-  print("error", sys.exc_info()[0])
-  raise
+    print(p.getPositionVelocity())
+    time.sleep(1)
+catch
+finally
