88f6f75cc819c12e47de799ea7a056aa838964f2
Clive Chan <cc@clive.io>
2017-03-03 23:59:23 -0500

    Waypointing almost works yarr

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 import socket
 import sys
 import time
+import math
 
 
 HOST, PORT = "codebb.cloudapp.net", 17429
-LOGIN = "duckduckgoose goosegooseduck"
+USERNAME, PASSWORD = "duckduckgoose", "goosegooseduck"
 #HOST, PORT = "10.20.42.146", 17429
-#LOGIN = "b b"
+#USERNAME, PASSWORD = "b", "b"
 
 """
 STATUS
@@ -18,42 +19,95 @@ SCOREBOARD
 CONFIGURATIONS
 """
 
-{
-  "player": (x,y),
-  "mines": []
-}
+def squaredDistance(pos1, pos2 = (0,0)):
+  return (pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2
+def neg(pos):
+  return (-pos[0], -pos[1])
+def add(pos1, pos2):
+  return (pos1[0] + pos2[0], pos1[1] + pos2[1])
+def scale(k, pos):
+  return (k*pos[0], k*pos[1])
+def sub(pos1, pos2):
+  return (pos1[0] - pos2[0], pos1[1] - pos2[1])
+def dot(pos1, pos2):
+  return pos1[0] * pos2[0] + pos1[1] * pos2[1]
+def proj(a, b):
+  factor = (dot(a,b) / squaredDistance(a))
+  return (factor * a[0], factor * a[1])
+def perp(a, b):
+  return sub(b, proj(a, b))
+def angle(pos):
+  return math.atan2(pos[1], pos[0])
+
 class Player:
-  def __init__(self, HOST, PORT, LOGIN):
+  def __init__(self, HOST, PORT, USERNAME, PASSWORD):
+    self.data = None
+    self.rawData = None
+    self.HOST = HOST
+    self.PORT = PORT
+    self.USERNAME = USERNAME
+    self.PASSWORD = PASSWORD
     self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.sock.connect((HOST, PORT))
-    print('LOGIN')
-    self.sock.send((LOGIN+'\n').encode())
-    print('success');
-  def getRawArr(self):
-    self.sock.send('STATUS\n'.encode())
+  def __enter__(self):
+    self.sock.connect((self.HOST, self.PORT))
+    self.sendCommand(self.USERNAME + ' ' + self.PASSWORD)
+    return self
+  def __exit__(self, exc_type, exc_value, traceback):
+    self.sock.close()
+  def sendCommand(self, str):
+    self.sock.send((str+'\n').encode())
+  def refreshData(self):
+    self.sendCommand('STATUS')
     data = self.sock.recv(4096)
     arr = repr(data).split(' ')
-    retval = dict()
-    retval["playerpos"] = (float(arr[1]), float(arr[2]))
-    retval["playervel"] = (float(arr[3]), float(arr[4]))
     
-    int(arr[6]) #number of mines
-    (float(arr[8+3*i]), float(arr[9+3*i]))
-    retval["mines"] = minearray
-  def getPositionVelocity(self):
-    arr = self.getRawArr()
-    return (float(arr[1]), float(arr[2]), float(arr[3]), float(arr[4]))
-  def getNearestMine(self):
-    arr = self.getRawArr()
-    return (float(
-  def waypoint(self, x, y): #fly through this point exactly. blocks until done.
-    ##while not passed through:
-    pass
+    self.rawData = arr
+    
+    processed = dict()
+    processed["pos"] = (float(arr[1]), float(arr[2]))
+    processed["vel"] = (float(arr[3]), float(arr[4]))
+    processed["mines"] = list()
+    processed["players"] = list()
+    processed["bombs"] = list()
+    
+    counter = 7
+    nummines = int(arr[counter])
+    for i in range(nummines): #x, y, owner (ONLY IF NOT OURS)
+      if arr[counter + 1 + 3*i] != self.USERNAME:
+        processed["mines"].append((float(arr[counter + 2 + 3*i]), float(arr[counter + 3 + 3*i]), arr[counter + 1 + 3*i]))
+    
+    counter += 2 + 3 * nummines
+    numplayers = int(arr[counter])
+    for i in range(numplayers): #x, y, dx, dy
+      processed["players"].append((float(arr[counter + 1 + 4*i]), float(arr[counter + 2 + 4*i]), float(arr[counter + 3 + 4*i]), float(arr[counter + 4 + 4*i])))
+    
+    counter += 2 + 4 * numplayers
+    numbombs = int(arr[counter])
+    for i in range(numbombs): #x, y
+      processed["bombs"].append((float(arr[counter + 1 + 2*i]), float(arr[counter + 2 + 2*i])))
+    
+    processed["mines"].sort(key = lambda x: squaredDistance(x, processed["pos"]))
+    processed["players"].sort(key = lambda x: squaredDistance(x, processed["pos"]))
+    processed["bombs"].sort(key = lambda x: squaredDistance(x, processed["pos"]))
+    
+    self.data = processed
+  def setAccel(self, angle, magnitude):
+    self.sendCommand("ACCELERATE " + str(angle) + " " + str(magnitude))
+  
+  def waypoint(self, target): #fly through this point exactly. blocks until done.
+    print("Waypointing to ", target)
+    p.refreshData()
+    diff = sub(target, self.data["pos"])
+    vel = self.data["vel"]
+    self.setAccel(angle(sub(vel, scale(2, perp(diff, vel)))), min(1, 0.1 + math.sqrt(squaredDistance(diff))/50))
 
 try:
-  p = Player(HOST, PORT, LOGIN)
-  while True:
-    print(p.getPositionVelocity())
-    time.sleep(1)
-catch
-finally
+  with Player(HOST, PORT, USERNAME, PASSWORD) as p:
+    while True:
+      p.refreshData()
+      if len(p.data["mines"]) > 0:
+        p.waypoint(sub(p.data["pos"], p.data["mines"][0]))
+      else:
+        p.setAccel(0.1, 1)
+except Exception as e:
+  print("Error", str(e))
